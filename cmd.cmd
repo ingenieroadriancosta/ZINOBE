ebf232aa6eb5af169c66a3248006834d7fb7b59c
adrian <adrianjocos@hotmail.com>
2020-09-19 01:41:07 -0500

    1.0

---
+++ cmd.cmd
@@ -0,0 +1,11 @@
+git init
+git status
+git add .
+git status
+git commit -m "1.0"
+git branch -M master
+git remote add origin https://github.com/ingenieroadriancosta/ZINOBE.git
+git push -u origin master
+
+
+
